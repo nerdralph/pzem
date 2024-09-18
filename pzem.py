62d654455b1c559b1fec8091d4272db4dbb60d9f
U-parsons-PC\parsons <ralphdoncaster@gmail.com>
2024-09-18 20:56:13 -0300

    untested -003 family support

--- pzem.py
+++ pzem.py
@@ -12,15 +12,36 @@ import serial.tools.list_ports as lp
 
 SADDR = 1                               # default slave address
 
+def modbus_connect(port: str=""):
+    if not port:
+        port = lp.comports()[0].device
+    modbus = ModbusSerialClient(port=port, baudrate=9600)
+    modbus.connect()
+    return modbus
+
+# read PZEM-003 family registers
+# return map of register values
+def regs_003(port: str="", addr: int=SADDR) -> dict:
+    modbus = modbus_connect(port)
+    # read all regs starting at address 0
+    rsp = modbus.read_input_registers(0, count=8, slave=addr)
+
+    regs={
+        "volt": rsp.registers[0]/100,
+        "amp": rsp.registers[1]/100,
+        "watt": rsp.registers[2]/10 + rsp.registers[3]*6553.6,
+        "watt-hour": rsp.registers[4] + rsp.registers[5]*65536,
+        "HValarm": rsp.registers[6],
+        "LValarm": rsp.registers[7]
+        }
+    return regs
+
 # read PZEM-004 family registers
 # return map of register values
 def regs_004(port: str="", addr: int=SADDR) -> dict:
-    if not port:
-        port = lp.comports()[0].device
-    rs485 = ModbusSerialClient(port=port, baudrate=9600)
-    rs485.connect()
-    # read 10 regs starting at address 0
-    rsp = rs485.read_input_registers(0, count=10, slave=addr)
+    modbus = modbus_connect(port)
+    # read all regs starting at address 0
+    rsp = modbus.read_input_registers(0, count=10, slave=addr)
 
     regs={
         "volt": rsp.registers[0]/10,
